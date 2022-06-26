9b0304d7be0b613bb84c3aee2bbb9d8b2d85e542
overcomer03 <outsaad03@gmail.com>
2022-06-26 18:15:38 +0000

    printf() with correct betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * Desctiption: print string
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
