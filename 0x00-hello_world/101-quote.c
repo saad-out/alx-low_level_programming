02f4e77f447f63380b2288a1c750d7d067e0f1f7
overcomer03 <outsaad03@gmail.com>
2022-06-30 11:57:10 +0000

    printing to standard error stream (correcting betty coding style)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	char s[100]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fwrite(s, sizeof(s), 1, stderr);
 	return (1);
 }
