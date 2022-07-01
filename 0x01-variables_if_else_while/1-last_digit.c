e5562cd1924ca35af4380d391cd60c4cf775cde7
overcomer03 <outsaad03@gmail.com>
2022-07-01 05:30:16 -0700

    locate last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,9 @@ int main(void)
 	int n, last_digit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2, last_digit = n % 10;
+	n = rand() - RAND_MAX / 2;
+	printf("%d n\n", n);
+	last_digit = n % 10;
 	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit == 0)
 		printf("and is 0\n");
