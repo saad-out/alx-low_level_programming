56e80f762d8fa6c40a5f8a67341987de1f3a5f6c
overcomer03 <outsaad03@gmail.com>
2022-07-05 03:53:34 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/* function prototype */
+void _putchar(char c);
+
+/**
+ * print_alphabet - print a-z
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return(0);
+}
