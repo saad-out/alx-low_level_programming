f3997ec541cca8605f76861fa8336daddcd20f8f
overcomer03 <outsaad03@gmail.com>
2022-07-07 07:51:17 +0000

    check if character is uppercase or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is uppercase or not
+ * @c: character to check
+ *
+ * Return: 1 if is uppercase | 0 if not
+ */
+int _isupper(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* header file inlcuding the write function*/
+#include <unistd.h>
+
+int _putchar(char c);
+
+int _isupper(char c);
+
+#endif
