e4561239a4baeca34289e4caea13f98511280e78
overcomer03 <outsaad03@gmail.com>
2022-07-14 14:10:00 +0000

    print number with _putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -18,5 +20,9 @@ char *cap_string(char *s);
 char *leet(char *s);
 
 char *rot13(char *s);
+
+int _putchar(char c);
+
+void print_number(int n);
 
 #endif
