00ed85e01ac7773a7fe2b4b5bf14b7457b5836cf
overcomer03 <outsaad03@gmail.com>
2022-07-14 10:29:51 +0000

    store letters in array

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,44 +1,26 @@
 #include "main.h"
 
 /**
- * leet - encoding a string into 1337
+ * leet - encode into 1337
  * @s: string given
  * Return: encoded string
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char l[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char r[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a')
-			s[i] = '4';
-		if (s[i] == 'e')
-			s[i] = '3';
-		if (s[i] == 'o')
-			s[i] = '0';
-		if (s[i] == 't')
-			s[i] = '7';
-		if (s[i] == 'l')
-			s[i] = '1';
-
-		i++;
-	}
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'L')
-			s[i] = '1';
-
+		j = 0;
+		while (j < 10)
+		{
+			if (s[i] == l[j])
+				s[i] = r[j];
+			j++;
+		}
 		i++;
 	}
 	return (s);
