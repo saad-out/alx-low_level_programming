3dd2e2282158b49194b2c63538cc48060cc09b9d
overcomer03 <outsaad03@gmail.com>
2022-07-18 13:47:25 +0000

    counting terminating char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,13 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0' && s[i] != c)
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
 		i++;
-
-	if (s[i] != '\0')
+	}
+	if (s[i] == c)
 		return (s + i);
-	else
-		return (0);
+	return (0);
 }
