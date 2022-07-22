da009be4e5e7f25b37120f5a07c5ed328665be60
overcomer03 <outsaad03@gmail.com>
2022-07-22 11:07:17 +0000

    addin __ after "attribute"

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: arguments vector (array of strings)
  * Return: always 0
  */
-int main(int argc, __attribute((unused)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
