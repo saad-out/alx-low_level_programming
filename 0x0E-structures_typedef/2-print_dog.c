67052ccf377483fed5db16c4e0c38ecc292cf24d
overcomer03 <outsaad03@gmail.com>
2022-08-01 01:56:05 -0700

    print struct dog items

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print struct dog items
+ * @d: pointer to struct dog variable to be printed
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: ");
+	if (d->name)
+		printf("%s\n", d->name);
+	else
+		printf("(nil)\n");
+
+	printf("Age: ");
+	if (d->age)
+		printf("%f\n", d->age);
+	else
+		printf("(nil)\n");
+
+	printf("Owner: ");
+	if (d->owner)
+		printf("%s\n", d->owner);
+	else
+		printf("(nil)\n");
+}
