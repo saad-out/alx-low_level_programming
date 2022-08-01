edd001431e70354afc32b5ebeb2e6eb742a61e6b
overcomer03 <outsaad03@gmail.com>
2022-08-01 02:10:04 -0700

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: pointer to new dog (Success) | NULL (Failure)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *tmp;
+
+	tmp = malloc(sizeof(dog_t));
+	if (!tmp)
+		return (0);
+	tmp->name = name, tmp->age = age, tmp->owner = owner;
+	return (tmp);
+}
