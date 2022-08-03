347c7cef684c00617644231e9b4f12b0cb973325
overcomer03 <outsaad03@gmail.com>
2022-08-03 10:27:09 +0000

    don't print empty string

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints name
@@ -8,7 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name)
+	if (!name || name[0] == '\0')
 		return;
 	f(name);
 }
