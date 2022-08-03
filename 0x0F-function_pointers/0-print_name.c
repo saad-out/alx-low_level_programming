a5f49905671cf879c0dc3ebb29a3df712fdabf77
overcomer03 <outsaad03@gmail.com>
2022-08-03 10:01:19 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: pointer to function to print name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
