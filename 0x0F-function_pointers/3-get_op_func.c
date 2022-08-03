994e0b07c981af3eb796f091b8d13d0fcf98288a
overcomer03 <outsaad03@gmail.com>
2022-08-03 16:01:53 +0000

    let get_of_func handle operator cases

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,12 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	i = 5;
-	while (i >= 0)
+	i = 0;
+	while (i <= 5)
 	{
 		if (ops[i].op && strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-		i--;
+		i++;
 	}
 	return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,26 +11,21 @@
 int main(int argc, char *argv[])
 {
 	int a, b, result;
-	char c;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	c = argv[2][0];
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((c != '+') && (c != '-') && (c != '*') && (c != '/') && (c != '%'))
+	func = get_op_func(argv[2]);
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	a = atoi(argv[1]), b = atoi(argv[3]);
-	result = (get_op_func(argv[2]))(a, b);
+	result = func(a, b);
 	printf("%d\n", result);
 	return (0);
 }
