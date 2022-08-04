2ef60262dcbc8991aca562197cb77abf43335f55
overcomer03 <outsaad03@gmail.com>
2022-08-04 02:22:38 -0700

    sum of all arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - calulate sum of all given parameters
+ * @n: first number (number of arguments)
+ * Return: sum (int)
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list p;
+
+	va_start(p, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(p, int);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef H
+#define H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* H */
