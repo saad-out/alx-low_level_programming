5112d7e31567029228db168b8870c6ebf5a68a06
overcomer03 <outsaad03@gmail.com>
2022-08-11 10:42:08 +0000

    add to end of list_t

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,6 +24,6 @@ size_t list_len(const list_t *h);
 
 list_t *add_node(list_t **head, const char *str);
 
-size_t _strlen(const char *s);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* H */
