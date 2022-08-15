630c93d8a3f58f4bd24b0dc4fb54c3e58546a6c9
overcomer03 <outsaad03@gmail.com>
2022-08-15 08:19:07 +0000

    free listint_t and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees listint_t and set head to NULL
+ * @head: head node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *next;
+
+	tmp = *head;
+	while (tmp)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*head = 0;
+}
