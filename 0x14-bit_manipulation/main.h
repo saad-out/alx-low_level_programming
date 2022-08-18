a729853fa78782f84d59fa8147f592dc55b089f7
overcomer03 <outsaad03@gmail.com>
2022-08-18 10:57:24 -0700

    set bit at index to 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,4 +9,9 @@ void print_binary(unsigned long int);
 
 int get_bit(unsigned long int, unsigned int);
 
+int set_bit(unsigned long int *, unsigned int);
+
+void convert_to_binary(unsigned long int, unsigned short *);
+
+unsigned long int convert_to_decimal(unsigned short *);
 #endif /* H */
