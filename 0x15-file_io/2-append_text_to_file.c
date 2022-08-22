bf2a127948137b7a0e2774518f33e651935f0fe5
saad-out <outsaad03@gmail.com>
2022-08-22 15:28:59 +0000

    append text to a file (if it exists)

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen2 - counts number of characters in a string
+ * @s: given string
+ *
+ * Return: Length of the string
+ */
+size_t _strlen2(char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: content to be added at the end of file
+ *
+ * Return: 1 (Success) | -1 (Failure)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+		if (write(fd, text_content, _strlen2(text_content)) < 0)
+			return (-1);
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,9 @@ int create_file(const char *filename, char *text_content);
 
 size_t _strlen(char *s);
 
+int append_text_to_file(const char *filename, char *text_content);
+
+size_t _strlen2(char *s);
+
+
 #endif /* H */
