3623070056e21e9777d761a8820325a17c8221fa
saad-out <outsaad03@gmail.com>
2022-08-22 21:33:47 +0000

    check if write prints the expected characters

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int ac, char **av)
 		if (i == -1)
 			dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 		j = write(fd_to, buffer, i);
-		if (j == -1)
+		if (j == -1 || j != i)
 			dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	} while (i);
 
