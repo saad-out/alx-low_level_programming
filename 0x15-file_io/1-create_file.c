3abed19a23b1f88d07ba89fa009a8f1d35dbe218
saad-out <outsaad03@gmail.com>
2022-08-22 14:51:40 +0000

    create (if it does not exist) and add content to a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - count number of characters in a string
+ * @s: given string
+ *
+ * Return: Length
+ */
+size_t _strlen(char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates file and adds text to it
+ * @filename: the name of the file
+ * @text_content: content to be added to file
+ *
+ * Return: 1 (Success) | -1 (Failure)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		if (write(fd, text_content, _strlen(text_content)) < 0)
+			return (-1);
+	close(fd);
+	return (1);
+}
