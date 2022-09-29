86fb550e3822b80f780f3d20edaa5032f5ca56d7
saad-out <outsaad03@gmail.com>
2022-09-29 12:22:41 +0000

    handle NULL argument cases

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,12 +14,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(*new_node));
 	if (!new_node)
 		return (NULL);
-	new_node->n = n, new_node->prev = NULL;
+	new_node->n = n, new_node->prev = NULL, new_node->next = NULL;
 
-	new_node->next = *head;
 	if (head && (*head))
+	{
 		(*head)->prev = new_node;
-	*head = new_node;
+		new_node->next = *head;
+	}
+	if (head)
+		*head = new_node;
 
 	return (new_node);
 }
