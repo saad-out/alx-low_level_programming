2616600e8dba7921d7ea44b35998de1b7ac228fc
saad-out <outsaad03@gmail.com>
2022-10-08 20:54:06 +0000

    free hash properly

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *node;
-	char *dup_value;
+	char *dup_value, *dup_key;
 
 	if (!ht || (!key || !(*key)) || !value)
 		return (0);
@@ -31,17 +31,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (!strcmp(node->key, key))
 		{
+			free(node->value);
 			node->value = dup_value;
 			return (1);
 		}
 		node = node->next;
 	}
 
+	dup_key = strdup(key);
+	if (!dup_key)
+	{
+		free(dup_value);
+		return (0);
+	}
+
 	/* otherwise, add new element */
 	node = malloc(sizeof(*node));
 	if (!node)
 		return (0);
-	node->key = (char *)key, node->value = dup_value;
+	node->key = dup_key, node->value = dup_value;
 	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
